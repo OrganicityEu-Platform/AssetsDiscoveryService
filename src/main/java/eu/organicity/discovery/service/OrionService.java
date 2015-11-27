47113ebb44b1303def1d0eda27fc1dbf66ce4555
Dimitrios Amaxilatis <d.amaxilatis@gmail.com>
2015-11-27 13:04:24 +0200

    check for a problem with the central CB and load data from the cite specific CBs

--- src/main/java/eu/organicity/discovery/service/OrionService.java
+++ src/main/java/eu/organicity/discovery/service/OrionService.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import javax.ws.rs.ProcessingException;
+import java.net.ConnectException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,40 +32,58 @@ public class OrionService {
     private static final Logger LOGGER = Logger.getLogger(OrionService.class);
 
 
-    private OrionClient orionClient;
+    private OrionClient centralOrion = new OrionClient("http://54.68.181.32:1026", "", "organicity", "/");
+    private OrionClient orionClientLondon = null;// new OrionClient("http://195.220.224.231:1026", "", "organicity", "/");
+    private OrionClient orionSantander = new OrionClient("http://mu.tlmat.unican.es:8099", "", "organicity", "/");
+    private OrionClient orionSmartphones = new OrionClient("http://195.220.224.231:1026", "", "organicity", "/");
+
     @Autowired
     DeviceService deviceService;
-
-    @PostConstruct
-    public void init() {
-//        orionClient = new OrionClient("http://195.220.224.231:1026", "");
-//        orionClient = new OrionClient("http://mu.tlmat.unican.es:8099", "");
-        orionClient = new OrionClient("http://54.68.181.32:1026", "", "organicity", "/");
-    }
 
     @Cachable(cacheName = "entities", expiration = 1)
     public List<Device> getEntities() {
         final List<Device> resources = new ArrayList<>();
 
         try {
-            ContextElementList entities;
-            long offset = 0;
-            do {
-                entities = orionClient.listContextEntities(offset);
-                offset += entities.getContextResponses().size();
-                for (final OrionContextElementWrapper orionContextElementWrapper : entities.getContextResponses()) {
-                    try {
-                        resources.add(deviceService.convert(orionContextElementWrapper.getContextElement()));
-                    } catch (Exception e) {
-                        LOGGER.error(e, e);
-                    }
-                }
-            } while (entities.hasMore(offset));
-
-            return resources;
-        } catch (Exception e) {
-            LOGGER.error(e, e);
+            findAll(resources, centralOrion);
+        } catch (Exception e1) {
+            LOGGER.warn("Could not connect to Central");
+            try {
+                findAll(resources, orionClientLondon);
+            } catch (Exception ignore) {
+                LOGGER.warn("Could not connect to London");
+            }
+            try {
+                findAll(resources, orionSantander);
+            } catch (Exception ignore) {
+                LOGGER.warn("Could not connect to Santander");
+            }
+            try {
+                findAll(resources, orionSmartphones);
+            } catch (Exception ignore) {
+                LOGGER.warn("Could not connect to Smartphones");
+            }
         }
-        return null;
+        return resources;
+    }
+
+
+    private void findAll(List<Device> resources, final OrionClient client) throws Exception {
+        if (client == null) {
+            return;
+        }
+        ContextElementList entities;
+        long offset = 0;
+        do {
+            entities = client.listContextEntities(offset);
+            offset += entities.getContextResponses().size();
+            for (final OrionContextElementWrapper orionContextElementWrapper : entities.getContextResponses()) {
+                try {
+                    resources.add(deviceService.convert(orionContextElementWrapper.getContextElement()));
+                } catch (Exception e) {
+                    LOGGER.error(e, e);
+                }
+            }
+        } while (entities.hasMore(offset));
     }
 }
